3ae440de75029c11f52d4586ee27bce7bc999c7e
Chris Zubak-Skees <chriszs@gmail.com>
2016-07-15 00:32:31 -0400

    Replace spaces with non-word characters, add optional "a" to "Pokemon"

--- content.js
+++ content.js
@@ -12,10 +12,10 @@ function troncIt() {
                 var text = node.nodeValue;
                 var replacedText = text
                                     .replace(/pokemongo/ig, 'goingoutside')
-                                    .replace(/(playing |played |play )?Pok(è|é|e)mon Go?/gi, 'going outside')
-                                    .replace(/Pok(é|e)mon?/gi, 'fresh air')
-                                    .replace(/lure module/gi, 'big tent')
-                                    .replace(/Pok(è|é|e)( )?stop/ig, 'place outside');
+                                    .replace(/(playing\W|played\W|play\W)?Pok(è|é|e)mon\WGo?/gi, 'going outside')
+                                    .replace(/(a\W)?Pok(é|e)mon?/gi, 'fresh air')
+                                    .replace(/lure\Wmodule/gi, 'big tent')
+                                    .replace(/Pok(è|é|e)(\W)?stop/ig, 'place outside');
 
                 if (replacedText !== text) {
                     element.replaceChild(document.createTextNode(replacedText), node);
