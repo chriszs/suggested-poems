0b7ca62913c2910419a3d377d538077b1966139e
Chris Zubak-Skees <chriszs@gmail.com>
2016-07-14 23:58:34 -0400

    Make some regex tweaks

--- content.js
+++ content.js
@@ -12,10 +12,10 @@ function troncIt() {
                 var text = node.nodeValue;
                 var replacedText = text
                                     .replace(/pokemongo/ig, 'goingoutside')
-                                    .replace(/(playing |played |play )?Pok(é|e)mon Go?/gi, 'going outside')
+                                    .replace(/(playing |played |play )?Pok(è|é|e)mon Go?/gi, 'going outside')
                                     .replace(/Pok(é|e)mon?/gi, 'fresh air')
-                                    .replace(/lure module?/gi, 'big tent')
-                                    .replace(/Pok(é|e)stop/ig, 'place outside');
+                                    .replace(/lure module/gi, 'big tent')
+                                    .replace(/Pok(è|é|e)stop/ig, 'place outside');
 
                 if (replacedText !== text) {
                     element.replaceChild(document.createTextNode(replacedText), node);
